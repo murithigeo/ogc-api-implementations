c0060c3ff5f6c4c3771cef94a535c994b56c75a5
kegeotech <murithiedwing@gmail.com>
2024-05-11 22:45:32 +0300

    Working on edr paths and schemas. Completed params

--- src/standards/edr/types.ts
+++ src/standards/edr/types.ts
@@ -93,7 +93,7 @@ interface Collection {
       link: DataQueryLinkDefault;
     };
     cube?: {
-      link: DataQueryLinkDefault;
+      link: DataQueryLinkDefault<{ height_units: string[] }>;
     };
     trajectory?: {
       link: DataQueryLinkDefault;
@@ -110,7 +110,7 @@ interface Collection {
     locations?: {
       link: DataQueryLinkDefault;
     };
-    //radius?: DataQueryRadius;
+    instances?: DataQueryLinkDefault;
   };
   crs: string[];
   output_formats: string[];
@@ -119,19 +119,21 @@ interface Collection {
   };
 }
 
+type QueryType =
+  | "instances"
+  | "position"
+  | "corridor"
+  | "cube"
+  | "trajectory"
+  | "area"
+  | "radius"
+  | "items"
+  | "locations";
 interface DataQueryLinkDefault<T = {}> extends Link {
   variables: {
     title: string;
     description: string;
-    query_type:
-      | "position"
-      | "corridor"
-      | "cube"
-      | "trajectory"
-      | "area"
-      | "radius"
-      | "items"
-      | "locations";
+    query_type: QueryType;
     default_output_format: default_output_format;
     crs_details: Crs_Details[];
   } & T;
@@ -214,8 +216,124 @@ interface ObservedProperty {
     }
   ];
 }
-interface CoverageJSON {}
+interface CoverageJSON {
+  type: "Coverage" | "CoverageCollection" | "Domain";
+  domainType?: string;
+  coverages: Coverage[];
+  parameters: {
+    [key: string]: EdrGeoJSONParameter;
+  };
+  ranges?: {
+    [key: string]: NdArray;
+  };
+  referencing?: ReferenceSystemConnection[];
+}
 
+interface Coverage {
+  type: "Coverage";
+  domain: Domain;
+  ranges: {
+    [key: string]: NdArray;
+  };
+}
+/**
+ * @interface NdArray description: Object representing a multidimensional (>= 0D) array with named
+  axes, encoded as a flat one-dimensional array in row-major order
+  @type axisNames. Items should be Unique
+  @type value. Unique=true. MinItems=1
+ */
+interface NdArray {
+  type: "NdArray";
+  dataType: "float" | "integer" | "string";
+  shape: number[];
+  axisNames: string[];
+  values: number[] | string[] | null;
+}
+interface Domain {
+  type: "Domain";
+  axes:
+    | {
+        x: NumericAxis;
+        y: NumericAxis;
+        z: NumericAxis;
+        t: NumericAxis;
+      }
+    | {
+        axes: {
+          composite: TupleValueAxis;
+          z?: NumericAxis;
+        };
+      };
+  referencing?: ReferenceSystemConnection[];
+}
+
+type ReferenceSystem =
+  | {
+      type: string;
+      calendar: string;
+      timeScale?: string;
+    }
+  | {
+      id: string;
+      label: i18N;
+      description: i18N;
+      targetConcept: { id?: string; label: i18N; description?: i18N };
+      identifiers?: {
+        [key: string]: { id?: string; label: i18N; description?: i18N };
+      };
+    };
+interface ReferenceSystemConnection {
+  coordinates: string[];
+  system: ReferenceSystem;
+}
+interface TupleValueAxis {
+  dataType: "tuple";
+  values: (string | number)[][];
+  coordinates: {};
+  bounds?: any[];
+}
+
+type ValuesAxis =
+  | ValuesAxisBase
+  | PrimitiveValuesAxis
+  | TupleValueAxis
+  | PolygonValuesAxis;
+
+interface PrimitiveValuesAxis extends ValuesAxisBase {
+  values: string[] | number[];
+  
+}
+
+interface PolygonValuesAxis {}
+
+type NumericAxis = NumericValuesAxis | NumericRegularlySpacedAxis;
+
+/**
+ * @interface NumericRegularlySpacedAxis
+ * @type num should be >1 & is int
+ */
+interface NumericRegularlySpacedAxis {
+  start: number;
+  stop: number;
+  num: number;
+}
+
+/**
+ * @interface ValuesAxisBase
+ * @type dataType !=="primitive"
+ * @type coordinates.length should be >1(minItems=2)
+ * @type bounds.length should be >1(minItems=2)
+ */
+type ValuesAxisBase = {
+  dataType: string;
+  values: any[];
+  coordinates?: string[];
+  bounds?: any[];
+};
+interface NumericValuesAxis extends ValuesAxisBase {
+  values: number[];
+  bounds: number[];
+}
 interface EdrGeoJSONFeatureCollection {
   type: "FeatureCollection";
   features: GeoJSONFeature[];
@@ -224,10 +342,10 @@ interface EdrGeoJSONFeatureCollection {
 
 interface GeoJSONFeature {
   type: "Feature";
-  id:string|number;
+  id: string | number;
   links?: Link[];
-  geometry: GeoJSONPoint|
-properties: edrGeoJSONProperties
+  geometry: GeoJSONPoint | GeoJSONGeometryCollection;
+  properties: edrGeoJSONProperties;
 }
 interface EdrGeoJSONParameter {
   id?: string;
@@ -278,22 +396,84 @@ interface GeoJSONPoint {
 
 interface GeoJSONGeometryCollection {
   type: "GeometryCollection";
-  geometries: 
+  geometries: GeoJSONPoint;
 }
 /**
  * @interface edrGeoJSONProperties
- * @example 
+ * @example
  */
-const edrGeoJSONProperties_Example: edrGeoJSONProperties={
+const edrGeoJSONProperties_Example: edrGeoJSONProperties = {
   datetime: "2020-10-10T12-02/2020-10-11",
-  "parameter-name": ["velocity","temperatue"],
-  label:"Site A",
-  edrqueryendpoint: "http://someURL/edd"
-}
+  "parameter-name": ["velocity", "temperatue"],
+  label: "Site A",
+  edrqueryendpoint: "http://someURL/edd",
+};
 
-interface edrGeoJSONProperties{
-  datetime: string;//Either a date-time or a period string that adheres to RFC 3339. Indicates the time instant or period for which data are available from the EDR feature.
+interface edrGeoJSONProperties {
+  datetime: string; //Either a date-time or a period string that adheres to RFC 3339. Indicates the time instant or period for which data are available from the EDR feature.
   "parameter-name": string[];
-  label:string;
+  label: string;
   edrqueryendpoint: string; //Is an URL
 }
+
+/**
+ * @interface LineStringGeoJSON
+ * @items Main array, 2ndArray MinItems=2. Nested Array minItems=2, max=4
+ * [number,number,?number,?number]
+ */
+
+interface LineStringGeoJSON {
+  type: "LineString" | "LineStringM" | "LineStringZ" | "LineStringZM";
+  coordinates: number[][][];
+}
+
+interface MultiPolygonGeoJSON {}
+
+/**
+ * @interface InstanceQuery
+ */
+interface InstanceQuery {
+  links: Link[];
+  instances: Collection[];
+}
+
+/**
+ * @interface ParameterGroup
+ */
+
+interface ParameterGroup {
+  type: "ParameterGroup";
+  id: string;
+  label: i18N;
+  description: i18N;
+  observedProperty: ObservedProperty;
+}
+
+/**
+ * @interface QueryParameterF
+ */
+interface QueryParameterF {
+  f: "json" | "yaml" | "coverageJSON" | "edrGeoJSON" | "featuresJSON";
+  type:
+    | "application/json"
+    | "text/yaml"
+    | "application/vnd.cov+json"
+    | "application/geo+json"
+    | "application/x-netcdf";
+}
+
+/**
+ * @interface AreaQueryResponse
+ */
+
+/**
+ * @interface InstancesResponse
+ * @type application/json
+ */
+
+interface InstancesResponse {
+  links: Link[];
+  instances: Collection[];
+}
+
+interface ItemsResponse {}
